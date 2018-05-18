81a9ed22c0ced3707daac4973a7fb40e9320ac99
ynp1994 <ynp2012@qq.com>
2018-05-18 21:16:26 +0800

    create pyliststack

---
+++ pyliststack.py
@@ -0,0 +1,14 @@
+class Stack:
+	def __init__(self):
+		self._theItems = list()
+	def isEmpty(self):
+		return len(self) == 0
+	def __len__(self):
+		return len(self._theItems)
+	def peek(self):
+		assert not self.isEmpty(), "Cannot peek at an empty stack"
+		return self._theItems[-1]
+	def pop(self):
+		assert not self.isEmpty(), "Cannot pop from an empty stack"
+	def push(self, item):
+		self._theItems.append(item)
